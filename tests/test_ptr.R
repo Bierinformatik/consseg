ca2f6225bbeb7294fdd1200a9c7e2864e29b6658
raim <raim@tbi.univie.ac.at>
2021-01-01 19:16:30 +0100

    testing xptr

---
+++ tests/test_ptr.R
@@ -0,0 +1,42 @@
+
+
+## PASS COMIPLED FUNCTION POINTER TO Rcpp FUNCTION
+## see https://gallery.rcpp.org/articles/passing-cpp-function-pointers/
+## also see https://gallery.rcpp.org/articles/passing-cpp-function-pointers-rcppxptrutils/
+
+
+library(Rcpp)
+library(RcppXPtrUtils)
+sourceCpp("test_ptr.cpp")
+
+## user-supplied function
+cpp <- "long double my_aeh(int L) { return (L*L*L/3); }"
+ptr <- cppXPtr(cpp)
+checkXPtr(ptr, type="long double", args=c("int")) # returns silently
+callViaXPtr(4, ptr)
+
+cpp <- "long double my_aeh(int L) { return (exp(L/2)-1); }"
+ptr <- cppXPtr(cpp)
+checkXPtr(ptr, type="long double", args=c("int")) # returns silently
+callViaXPtr(4, ptr)
+
+## pre-compiled function
+ptr <- putFunPtrInXPtr("aeh")
+callViaXPtr(4, ptr)
+
+## error check
+cpp <- "double my_aeh(int L) { return (exp(L/2)-1); }"
+ptr <- cppXPtr(cpp)
+tmp <- try(checkXPtr(ptr, type="long double", args=c("int")))
+if ( !"try-error"%in%class(tmp) ) {
+    callViaXPtr(4, ptr)
+}
+
+cpp <- "long double my_aeh(int L) { return (exp(L/2)-1); }"
+ptr <- cppXPtr(cpp)
+tmp <- try(checkXPtr(ptr, type="long double", args=c("int")))
+if ( !"try-error"%in%class(tmp) ) {
+    callViaXPtr(4, ptr)
+}
+
+
